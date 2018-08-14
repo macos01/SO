50968097eb291068d4e39ba74b6dbda3b4dc908e
Marcos Calero <mcaler01@ucm.es>
2018-08-14 23:44:21 +0200

    Fixes

---
+++ Win32 API Desktop Application/fact.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <ostream>
+
+
+void list_files_recursive(std::string init_path, HDC &hdc,int* x, int* y);
